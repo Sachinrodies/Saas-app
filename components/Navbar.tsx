98c309ee967f5873641a400eaf47eeaa97bc4acb
sachin jha <sahilrodies000@gmail.com>
2025-06-21 08:27:20 +0530

    Update Navbar component styling and layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,53 +1,34 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import Navitems from './Navitems'
-import {
-  SignInButton,
-  SignOutButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import NavItems from "@/components/Navitems";
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-        <Link href="/">
-        <div className="flex items-center gap 2.5 cursor-pointer">
-            <Image src="/images/logo.svg" alt="logo" width={46} height={44} />
-
-        </div>
-          
-        </Link>
-        <div className="flex items-center gap-8">
-            <Navitems/>
-           <SignedOut>
-            
-              <SignInButton>
-                <button className="btn-signin">
-                  Sign In
-                </button>
-              </SignInButton>
-
-           
-           </SignedOut>
-           <SignedIn>
-            <div className="flex items-center gap-4">
-              <UserButton/>
-              <SignOutButton>
-                <button className="btn-signin">
-                  <Image src="/icons/logout.svg" alt="logout" width={16} height={16} />
-                  Sign Out
-                </button>
-              </SignOutButton>
+    return (
+        <nav className="navbar">
+            <Link href="/">
+                <div className="flex items-center gap-2.5 cursor-pointer">
+                    <Image
+                        src="/images/logo.svg"
+                        alt="logo"
+                        width={46}
+                        height={44}
+                    />
+                </div>
+            </Link>
+            <div className="flex items-center gap-8">
+                <NavItems />
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-signin">Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </div>
-           </SignedIn>
-
-        </div>
-
-    </nav>
-  )
+        </nav>
+    )
 }
 
 export default Navbar
